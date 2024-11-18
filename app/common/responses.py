6c9d505d2a788f289db5a6b01f7795535fe6c9e7
Hristiyan <hristiyandudev@gmail.com>
2024-11-18 23:41:30 +0200

    Updated database models, final models ( we hope so), also updated responses - returning json.

--- app/common/responses.py
+++ app/common/responses.py
@@ -1,41 +1,42 @@
 from fastapi import Response
+from typing import Any
 
 
 class Complete(Response):
     def __init__(self):
-        super().__init__(status_code=200)
+        super().__init__(status_code=200, media_type="application/json")
 
 
 class Accepted(Response):
-    def __init__(self, content=''):
-        super().__init__(status_code=202, content=content)
+    def __init__(self, content: Any = None):
+        super().__init__(status_code=202, content=content, media_type="application/json")
 
 
 class NoContent(Response):
     def __init__(self):
-        super().__init__(status_code=204)
+        super().__init__(status_code=204, media_type="application/json")
 
 
 class BadRequest(Response):
-    def __init__(self, content=''):
-        super().__init__(status_code=400, content=content)
+    def __init__(self, content: Any = None):
+        super().__init__(status_code=400, content=content, media_type="application/json")
 
 
 class Unauthorized(Response):
-    def __init__(self, content=''):
-        super().__init__(status_code=401, content=content)
+    def __init__(self, content: Any = None):
+        super().__init__(status_code=401, content=content, media_type="application/json")
 
 
 class Forbidden(Response):
-    def __init__(self, content=''):
-        super().__init__(status_code=403, content=content)
+    def __init__(self, content: Any = None):
+        super().__init__(status_code=403, content=content, media_type="application/json")
 
 
 class NotFound(Response):
-    def __init__(self, content=''):
-        super().__init__(status_code=404, content=content)
+    def __init__(self, content: Any = None):
+        super().__init__(status_code=404, content=content, media_type="application/json")
 
 
 class InternalServerError(Response):
-    def __init__(self):
-        super().__init__(status_code=500)
+    def __init__(self, content: Any = None):
+        super().__init__(status_code=500, content=content, media_type="application/json")
